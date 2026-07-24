a09e223bb274a9f0247ea85421590f37144185e6
Narek <yfhtr.stepanyan@mail.ru>
2026-07-24 20:02:30 +0400

    Создал макет главного экрана

--- app/src/main/java/ru/lilnaro/finflow/presentation/home/HomeAction.kt
+++ app/src/main/java/ru/lilnaro/finflow/presentation/home/HomeAction.kt
@@ -1,2 +1,12 @@
 package ru.lilnaro.finflow.presentation.home
 
+sealed interface HomeAction {
+
+    data object TransactionsClicked : HomeAction
+
+    data object AssistantClicked : HomeAction
+
+    data object NewMonthClicked : HomeAction
+
+    data object ArchiveClicked : HomeAction
+}
